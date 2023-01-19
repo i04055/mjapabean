ba55b46380c563db4191dab4599c2191039347c5
Rio <r_thelight@yahoo.com>
2023-01-19 09:24:32 +0700

    remove success resgister

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -42,12 +42,6 @@
                     <form action="/" method="post">
                         @csrf
         
-                        @if(session()->has('success'))
-                        <div class="alert alert-success alert-dismissible fade show" role="alert">
-                            <strong>{{ session('success') }}</strong>
-                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                        </div>
-                        @endif
                         @if(session()->has('loginError'))
                         <div class="alert alert-danger alert-dismissible fade show" role="alert">
                             <strong>{{ session('loginError') }}</strong>
